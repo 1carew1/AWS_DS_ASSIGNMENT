d19c20eefe3a2d42fd25db9392c4e3c25057521d
Colm Carew <colm.carew@errigal.com>
2016-11-15 21:50:24 +0000

    Creating Security Groups from bash script

--- aws_scripts.sh
+++ aws_scripts.sh
@@ -6,5 +6,14 @@ My_CURRENT_IP=$(wget http://ipinfo.io/ip -qO -)
 WEB_SERVER_SECUTIY_GROUP="WServerSG"
 DB_SECURITY_GRPUP="DatabaseSG"
 
-aws ec2 create-security-group --group-name $WEB_SERVER_SECUTIY_GROUP --description "Web Servcer SG" --vpc-id $VPC_ID
-aws ec2 create-security-group --group-name $DB_SECURITY_GRPUP --description "Database SG" --vpc-id $VPC_ID
+# Set Up Web Server SG - allow port 22, 80, 8080 and 443 from this IP
+WEB_SG_ID=$(aws ec2 create-security-group --group-name $WEB_SERVER_SECUTIY_GROUP --description "Web Servcer SG" --vpc-id $VPC_ID | grep GroupId | sed  's/"GroupId": "//g' | sed 's/"//g' | sed 's/"//g' )
+aws ec2 authorize-security-group-ingress --group-id $WEB_SG_ID --protocol tcp --port 22 --cidr $My_CURRENT_IP/32
+aws ec2 authorize-security-group-ingress --group-id $WEB_SG_ID  --protocol tcp --port 80 --cidr $My_CURRENT_IP/32
+aws ec2 authorize-security-group-ingress --group-id $WEB_SG_ID  --protocol tcp --port 443 --cidr $My_CURRENT_IP/32
+aws ec2 authorize-security-group-ingress --group-id $WEB_SG_ID  --protocol tcp --port 8080 --cidr $My_CURRENT_IP/32
+
+# Set Up DB SG - allow ssh from Web Server
+DB_SG_ID=$(aws ec2 create-security-group --group-name $DB_SECURITY_GRPUP --description "Database SG" --vpc-id $VPC_ID | grep GroupId | sed  's/"GroupId": "//g' | sed 's/"//g' | sed 's/"//g' )
+aws ec2 authorize-security-group-ingress --group-id $DB_SG_ID  --protocol tcp --port 22 --source-group $WEB_SG_ID
+
